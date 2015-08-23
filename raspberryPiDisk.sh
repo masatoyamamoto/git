56e24249d9b2d9ce6a394558a3201aed81e4fffd
masatoyamamoto <masato.yamamoto10@gmail.com>
2015-08-23 20:10:43 +0900

    delete

--- raspberryPiDisk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-diskutil info /dev/rdisk1
-diskutil unmount /dev/disk1s1
-sudo dd if=~/Documents/computer/raspberry\ pi/2012-12-16-wheezy-raspbian.img of=/dev/rdisk1 bs=1m
-diskutil unmount /dev/disk1s1
